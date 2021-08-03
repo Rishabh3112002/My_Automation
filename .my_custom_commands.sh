b99df57973cfbb258ae419f974a247770c19e217
Rishabh3112002 <65342857+Rishabh3112002@users.noreply.github.com>
2021-08-03 16:30:11 +0530

    Update .my_custom_commands.sh

--- .my_custom_commands.sh
+++ .my_custom_commands.sh
@@ -5,7 +5,7 @@ function create(){
   python mycode.py $1
   cd C:/Users/ksr20/PycharmProjects/$1
   git init
-  git remote add origin https://github.com/Rishabh3112002/$1.git
+  git remote add origin https://github.com/YOUR_GITHUB_USERNAME/$1.git
   touch README.md
   git add .
   git commit -m "Initial commit"
